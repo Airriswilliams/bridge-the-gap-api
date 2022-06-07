d6d510093a14569fc18232035af847f15bddf81b
Airris Williams <airriswilliams83@gmail.com>
2022-06-07 15:44:22 -0500

    fixtures

--- bridgethegapapi/models/tutor.py
+++ bridgethegapapi/models/tutor.py
@@ -5,4 +5,4 @@ class Tutor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=100) 
     schedule = models.CharField(max_length=100)
-    image_url = models.CharField(max_length=300)
+    image_url = models.URLField(max_length=1000)
